7bd59d97a9f01471a7dcdef321e3de63c39ac748
Valentin Beaussart <valentin.beaussart@gmail.com>
2025-06-04 22:00:29 +0200

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [tailwind()],
   middleware: ['src/middleware/auth.ts'],
   output: 'server',
   compressHTML: true,
+  adapter: netlify(),
   build: {
     inlineStylesheets: 'always',
     minify: true,
